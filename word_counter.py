673d9221a8210031852d98a87b45ed978f07ce27
Joseph Mellor <the.landfill.coding@gmail.com>
2020-02-27 12:02:52 -0600

    Sanitized Data for Better Analysis
    1.	If you looked at the output file (specifically near the bottom of the file),
    	you would see that we would count two words that are clearly the same word
    	("THEE and THEE and thee and thee.) as separate words because 'E' and 'e'
    	are different characters and we're breaking words apart using spaces,
    	meaning "thee and thee would be considered different because they're
    	different strings.
    
    2.	To fix this, we're going to use re.sub(), which uses a pattern known as a
    	regular expression to determine what text to substitute. First, we need to
    	import re (line 2) so we can use the re module. Then, we need to create a
    	new variable to store the cleaned line (cleaned_lin). Then, we call the
    	re.sub function, which has the syntax
    
    		re.sub(what_to_replace, what_to_replace_it_with, what_to_read_from)
    
    	In our case, we're using
    
    		re.sub(r"[^a-zA-Z0-9\s]", "", line)
    
    	which means we're going to read from line and replace anything that matches
    	the pattern r"[^a-zA-Z0-9\s]" with the empty string "", effectively removing
    	it. You can read the regular expression as
    
    		r"                # r" means we don't have to escape the \ character
    		    [^            # Everything but the following characters
    		        a-z       # All characters from unicode a to unicode z
    		                  # (lowercase letters)
    		        A-Z       # All characters from unicode A to unicode Z
    		                  # (uppercase letters)
    		        0-9       # All characters from unicode 0 to unicode 9 (digits)
    		        \s        # Any whitespace character (spaces, tabs, newlines,
    		                  # etc.)
    		    ]             # Close square bracket
    		"                 # Close string
    
    		In other words, replace every character that isn't a letter, digit, or
    		whitespace with "".
    
    3.	re.sub returns a string without modifying line, and we want the and The to
    	be considered the same word, so let's make them lowercase using lower(),
    	which has the syntax
    
    		example_sting.lower()
    
    4.	Then we split the clean_line just like we used to split line.

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 
 def save_word_counts_to_file(word_counts, word_count_file):
     with open(word_count_file, "w") as writer:
@@ -9,7 +10,8 @@ def count_words(filename):
     word_counts = {}
     with open(filename, "r") as reader:
         for line in reader:
-            words = line.split()
+            clean_line = re.sub(r"[^a-zA-Z0-9\s]", "", line).lower()
+            words = clean_line.split()
             for word in words:
                 if word in word_counts:
                     word_counts[word] += 1
