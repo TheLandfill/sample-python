bd9ff9754dbacdb3171b7c793a73a8cf9e7d912f
Joseph Mellor <the.landfill.coding@gmail.com>
2020-02-27 13:12:40 -0600

    Using scipy to Generate a Fit for Our Data
    1.	We're going to use an external library known as scipy to generate a fit for
    	our data. To install it from the terminal (Linux, Mac, or Windows Subsystem
    	for Linux), we can use the command
    
    		pip3 install scipy
    
    	Once it's installed, we can use it in our code in a similar way to how we
    	used the re module.
    
    		from scipy.optimize import curve_fit as fit
    
    	Doing so will let us take the function curve_fit in the scipy.optimize
    	submodule and rename it to fit.
    
    2.	We also created a new function called pareto_fit, which will try to fit the
    	Pareto distribution (the continuous version of the Zipf distribution) to our
    	data. Since we haven't normalized our data (divided by the total number of
    	words), we can't use the normal Pareto distribution. Instead, I have
    	modified it to better fit the data.
    
    3.	Only the ** operator in the pareto function is new, and it's just "to the
    	power of".
    
    		2 ** 10 is 1024
    		25 ** 0.5 is 5.
    
    4.	For fit, we need a function, x values, and y values. We already have our
    	function (pareto) and we already have our y values (counts), so we just need
    	our x values, which is the rank of the word in the frequency table.
    	range(a, b) generates a range that goes from a to b - 1, and we need to
    	convert it to a list since fit expects a list.
    
    5.	fit returns a tuple containing two values, an array for what it considers to
    	be the best parameters and the covariance matrix. In our case, we only care
    	about the diagonal elements of the covariance matrix since they're the
    	variances of each variable, which is a measure of how spread out the data
    	is (and can also be considered a measure of error). We can get the results
    	using
    
    		popt, pcov = fit(function, x_vals, y_vals)
    
    	Lastly, we can set p0 equal to what we would guess the parameters would be
    	to help it out. As long as we're in the ballpark, we'll be fine. Natural
    	languages have an alpha close to one and x_m should be near the count of the
    	most common word raised to the (1 / alpha) power, so guessing the count of
    	the most common word should get us in the ballpark.
    
    6.	perc_std_dev is another lambda that returns the standard deviation as a
    	percentage of the actual value converted to a string.
    
    7.	Next, we're going to print out the parameters with their standard deviation
    	as a percentage of the parameter. The "\t" is a tab character, the u in
    	front of the quotes means read the string in Unicode, and \u00B1 is the plus
    	minus symbol.
    
    8.	We return popt, which contains the parameters so we can graph the function.
    
    9.	Lastly, we call the function in main(). Since we just need to get the counts
    	of the words, we use list comprehension for the statement
    
    		counts = [k[1] for k in sorted_word_counts]
    
    	List comprehension uses the syntax
    
    		[some_func(k) for k in list]
    
    	and returns a list containing some_func applied to all the elements of k.
    	For example, if we wanted to add 7 to each element of a list named example,
    	we could do
    
    		[k + 7 for k in example]
    
    	In our case, we just wanted the second element of each element in
    	sorted_word_counts, which we can access by using k[1].

--- word_counter.py
+++ word_counter.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import re
+from scipy.optimize import curve_fit as fit
 
 def save_word_counts_to_file(word_counts, word_count_file):
     with open(word_count_file, "w") as writer:
         for word in word_counts:
             writer.write(word[0] + " " + str(word[1]) + "\n")
+
+def pareto(x, x_m, alpha):
+    return (x_m / x) ** alpha
+
+def pareto_fit(counts):
+    word_ranks = list(range(1, len(counts) + 1))
+    popt, pcov = fit(pareto, word_ranks, counts, p0=[counts[0], 1])
+    perc_std_dev = lambda var, val: str(100 * var ** 0.5 / val) + "%"
+    print("x_m:\t" + str(popt[0]) + u" \u00B1 " + perc_std_dev(pcov[0][0], popt[0]))
+    print("alpha:\t" + str(popt[1]) + u" \u00B1 " + perc_std_dev(pcov[1][1], popt[1]))
+    return popt
 
 def count_words(filename):
     word_counts = {}
@@ -23,6 +35,8 @@ def main():
     word_counts = count_words("sample-text.txt")
     sorted_word_counts = sorted(word_counts.items(), key = lambda x: -x[1])
     save_word_counts_to_file(sorted_word_counts, "word-counts.txt")
+    counts = [k[1] for k in sorted_word_counts]
+    popt = pareto_fit(counts)
 
 if __name__ == "__main__":
     main()
